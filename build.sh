1894b9711f28438c7c85fdc00a2064a5407ec5b3
vradko <justslavic@gmail.com>
2025-03-30 19:30:53 +0400

    Update Mac M1 branch predictor test

--- build.sh
+++ build.sh
@@ -6,20 +6,22 @@ DEFINES="-DDEBUG_PROFILE=1 -DOS_MAC"
 
 INCLUDES="-I../code"
 
-gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o generate_data ../code/generate_data.c
-gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o compute ../code/compute.c
-gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o rep_test ../code/repetition_testing.c
+mkdir -p build
 
-gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o 01_test_fread ../code/01_test_fread.c
-gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o 02_test_pagefault_count ../code/02_test_pagefault_count.c
+# gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/generate_data ../code/generate_data.c
+# gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/compute ../code/compute.c
+# gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/rep_test ../code/repetition_testing.c
+
+gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/01_test_fread code/01_test_fread.c
+gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/02_test_pagefault_count code/02_test_pagefault_count.c
 
 # ld abc.o -o abc -e _start -arch arm64  -l System -syslibroot `xcrun -sdk macosx --show-sdk-path`
 
-as ../code/03_test_cpu_frontend.arm64 -o 03_asm.o
-gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o 03_test_cpu_frontend ../code/03_test_cpu_frontend.c 03_asm.o
+as code/03_test_cpu_frontend.arm64 -o 03_asm.o
+gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o build/03_test_cpu_frontend code/03_test_cpu_frontend.c build/03_asm.o
 
-as ../code/04_test_decoding_bottleneck.arm64 -o 04_asm.o
-gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o 04_test_decoding_bottleneck ../code/04_test_decoding_bottleneck.c 04_asm.o
+as code/04_test_decoding_bottleneck.arm64 -o build/04_asm.o
+gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o build/04_test_decoding_bottleneck code/04_test_decoding_bottleneck.c build/04_asm.o
 
-
-# as ../code/04_test_rat.arm64 -o 03_rat.o
+as code/05_test_branch_predictor.arm64 -o build/05_asm.o
+gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o build/05_test_branch_predictor code/05_test_branch_predictor.c build/05_asm.o
