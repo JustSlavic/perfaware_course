f66ebfdc9f18380acba7f2a6c9e356f35d9bc7e7
vradko <justslavic@gmail.com>
2025-03-30 23:17:12 +0400

    Test RAT on M1

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 C_FLAGS="-std=c11 -g"
 WARNINGS="-Wall -Werror"
 DEFINES="-DDEBUG_PROFILE=1 -DOS_MAC"
@@ -8,20 +10,29 @@ INCLUDES="-I../code"
 
 mkdir -p build
 
+function compile_test_arm64_mac() {
+    assembly_source_name="code/$1.arm64"
+    assembly_object_name="build/$1_asm.o"
+    source_name="code/$1.c"
+    executable_name="build/$1"
+    as $assembly_source_name -o $assembly_object_name
+    echo "Done [$assembly_object_name]"
+    gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o $executable_name $source_name $assembly_object_name
+    echo "Done [$executable_name]"
+}
+
 # gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/generate_data ../code/generate_data.c
 # gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/compute ../code/compute.c
 # gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/rep_test ../code/repetition_testing.c
 
-gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/01_test_fread code/01_test_fread.c
-gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/02_test_pagefault_count code/02_test_pagefault_count.c
+# gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/01_test_fread code/01_test_fread.c
+# echo "Done [build/01_test_fread]"
+# gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/02_test_pagefault_count code/02_test_pagefault_count.c
+# echo "Done [build/02_test_pagefault_count]"
 
 # ld abc.o -o abc -e _start -arch arm64  -l System -syslibroot `xcrun -sdk macosx --show-sdk-path`
 
-as code/03_test_cpu_frontend.arm64 -o 03_asm.o
-gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o build/03_test_cpu_frontend code/03_test_cpu_frontend.c build/03_asm.o
-
-as code/04_test_decoding_bottleneck.arm64 -o build/04_asm.o
-gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o build/04_test_decoding_bottleneck code/04_test_decoding_bottleneck.c build/04_asm.o
-
-as code/05_test_branch_predictor.arm64 -o build/05_asm.o
-gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o build/05_test_branch_predictor code/05_test_branch_predictor.c build/05_asm.o
+compile_test_arm64_mac "03_test_cpu_frontend"
+compile_test_arm64_mac "04_test_decoding_bottleneck"
+compile_test_arm64_mac "05_test_branch_predictor"
+compile_test_arm64_mac "06_test_rat"
---
+++ code/06_test_rat.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "base.h"
+#include "os_specifics.c"
+
+#undef bool
+
+
+#include "timing.h"
+#include "profiler.h"
+#include "repetition_tester.h"
+
+/*
+    This test designed to show the work of the RAT (Register Allocation Table (?))
+
+    The first test does two adds in a row, which make the first dependency chain.
+    The second dependency chain is on 'dec'.
+
+        add 0  dec
+          |     |
+        add 1   |
+          |     |
+        add 0  dec
+          |     |
+        ...  ...
+
+    But the second example does mov before add, so although the
+    name of the register is the same, the RAT makes sure that
+    computation happens in parallel:
+
+        mov 0  mov 1  dec
+          |      |     |
+        add 0  add 1  dec
+          |      |     |
+         ...   ...    ...
+
+    Arm64
++-------------------+-----+-------------------+-------------------+----------------------+
+| Label             |     | Time              | Bytes             | Page faults          |
++-------------------+-----+-------------------+-------------------+----------------------+
+| rat_add_asm       | Min | 674476 us         | 1.482632 gb/s     |                      |
+|                   | Max | 702283 us         | 1.423927 gb/s     |                      |
+|                   | Avg | 675849 us         |                   |                      |
++-------------------+-----+-------------------+-------------------+----------------------+
+| rat_mov_add_asm   | Min | 337379 us         | 2.964026 gb/s     |                      |
+|                   | Max | 342224 us         | 2.922063 gb/s     |                      |
+|                   | Avg | 338327 us         |                   |                      |
++-------------------+-----+-------------------+-------------------+----------------------+
+
+    The result on M1 CPU clearly shows that although using moves
+    is seemingly more work, the dependency becomes "fake" and now
+    computation happens two times faster, which aligns with our
+    mental model of the CPU.
+*/
+
+typedef void (*callback_t)(uint64);
+
+void test_rat_add_asm(uint64 count);
+void test_rat_mov_add_asm(uint64 count);
+
+char const *labels[] =
+{
+    "rat_add_asm",
+    "rat_mov_add_asm",
+};
+
+callback_t callbacks[] =
+{
+    test_rat_add_asm,
+    test_rat_mov_add_asm,
+};
+
+void reptest_do(int index, uint64 count)
+{
+    g_tester.label = labels[index];
+    while (is_testing(60))
+    {
+        reptest_begin_time();
+        callbacks[index](count);
+        reptest_end_time();
+        reptest_count_bytes(count);
+    }
+}
+
+int main()
+{
+    uint64 count = GIGABYTES(1);
+    g_tester.print_results = true;
+    // while (true)
+    {
+        for (int i = 0; i < ARRAY_COUNT(callbacks); i++)
+        {
+            reptest_do(i, count);
+        }
+    }
+    return 0;
+}
+
+
+#include "timing.c"
+#include "profiler.c"
+#include "repetition_tester.c"
