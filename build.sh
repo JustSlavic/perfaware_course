087506d43e6b3cb7df81f401384f350a99aeac0d
vradko <justslavic@gmail.com>
2025-09-05 01:51:04 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ INCLUDES="-I../code"
 mkdir -p build
 
 function compile_test_arm64_mac() {
+    source_name="code/$1.c"
+    executable_name="build/$1"
+    clang $C_FLAGS -O2 $WARNINGS $DEFINES $INCLUDES -o $executable_name $source_name
+    echo "Done [$executable_name]"
+}
+
+function compile_test_arm64_mac_asm() {
     assembly_source_name="code/$1.arm64.S"
     assembly_object_name="build/$1_asm.o"
     source_name="code/$1.c"
@@ -18,7 +25,7 @@ function compile_test_arm64_mac() {
     as $assembly_source_name -o $assembly_object_name
     echo "Done [$assembly_object_name]"
 
-    gcc $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o $executable_name $source_name $assembly_object_name
+    clang $C_FLAGS -O1 $WARNINGS $DEFINES $INCLUDES -o $executable_name $source_name $assembly_object_name
     echo "Done [$executable_name]"
 }
 
@@ -53,31 +60,23 @@ if [[ $os_name == "Linux" ]]; then
 elif [[ $os_name == "Darwin" ]]; then
     DEFINES="-DDEBUG_PROFILE=1 -DOS_MAC"
 
-    # gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/generate_data ../code/generate_data.c
-    # gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/compute ../code/compute.c
-    # gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/rep_test ../code/repetition_testing.c
+    compile_test_arm64_mac "00_generate_haversine_data"
+    compile_test_arm64_mac "00_compute_haversine"
+    compile_test_arm64_mac "01_test_fread"
+    compile_test_arm64_mac "02_test_pagefault_count"
 
-    # gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/01_test_fread code/01_test_fread.c
-    # gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/02_test_pagefault_count code/02_test_pagefault_count.c
+    compile_test_arm64_mac_asm "03_test_cpu_frontend"
+    compile_test_arm64_mac_asm "04_test_decoding_bottleneck"
+    compile_test_arm64_mac_asm "05_test_branch_predictor"
+    compile_test_arm64_mac_asm "06_test_rat"
+    compile_test_arm64_mac_asm "07_test_one_byte_load"
+    compile_test_arm64_mac_asm "08_test_execution_ports"
+    compile_test_arm64_mac_asm "09_test_simd_load"
+    compile_test_arm64_mac_asm "10_test_cache_size"
+    compile_test_arm64_mac_asm "11_test_unaligned_load"
 
-    # ld abc.o -o abc -e _start -arch arm64  -l System -syslibroot `xcrun -sdk macosx --show-sdk-path`
-
-    compile_test_arm64_mac "03_test_cpu_frontend"
-    compile_test_arm64_mac "04_test_decoding_bottleneck"
-    compile_test_arm64_mac "05_test_branch_predictor"
-    compile_test_arm64_mac "06_test_rat"
-    compile_test_arm64_mac "07_test_one_byte_load"
-    compile_test_arm64_mac "08_test_execution_ports"
-    compile_test_arm64_mac "09_test_simd_load"
-    compile_test_arm64_mac "10_test_cache_size"
-    compile_test_arm64_mac "11_test_unaligned_load"
-
-    gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/generate_data code/generate_data.c
-    echo "Done [build/generate_data]"
-    gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/12_test_math_functions_domain code/12_test_math_functions_domain.c
-    echo "Done [build/12_test_math_functions_domain]"
-    gcc $C_FLAGS $WARNINGS $DEFINES $INCLUDES -o build/13_test_math_functions_error code/13_test_math_functions_error.c
-    echo "Done [build/13_test_math_functions_error]"
+    compile_test_arm64_mac "12_test_math_functions_domain"
+    compile_test_arm64_mac "13_test_math_functions_error"
 else
     echo "Could not recognize the system I'm on! (${os_name})"
     exit 1
